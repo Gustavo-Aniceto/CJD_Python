08405a2d0e45bee84d2f199d515f0eef80059bac
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-02-08 00:49:10 -0300

    primeiro exercicio

---
+++ Adivinha.py
@@ -0,0 +1,20 @@
+import random
+import os
+
+erros = 0
+sorteado = random.randrange(0,100)
+jogador = int(input("Digite seu numero:"))
+
+while(sorteado!=jogador):
+    os.system('cls')
+    if(sorteado>jogador):
+        print("ERRO, o nemero é maior")
+    elif (sorteado < jogador):
+            print("ERRO, o nemero é Menor")
+
+    erros+=1
+    jogador = int(input("Digite seu numero:"))
+
+print(" Numero " + str (jogador)+", voce acertou em " + str (erros+1)+" tentativas ")
+
+
